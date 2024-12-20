8461194acb01f5fef4f3aa790df32162a607ea04
UzairGhori <ughori435@gmail.com>
2024-12-20 23:48:01 +0500

    first commit

--- src/app/Component/figma/Navbar/page.tsx
+++ src/app/Component/figma/Navbar/page.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             alt='logo'
             width={191}
             height={34}
-            className='w-[191px] h-[34px] '
+            className='w-[191px] h-[34px] items-center '
           />
           
         </div> 
